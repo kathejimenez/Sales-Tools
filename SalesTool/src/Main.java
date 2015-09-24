85295daef7e13bb4a5c81e435efd67b3ed2c9886
Katherine <20121151@itla.edu.do>
2015-09-23 21:05:24 -0700

    Initial checking of sales tools..

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+       data.display();
+	   displayMeeting();
+		
+	}
+	
+	private static void displayMeeting(){
+		System.out.println("Digno y Santo");
+		System.out.println("Es Dios!");
+		
+		
+	}
+
+}
---
+++ SalesTool/src/SalesData.java
@@ -0,0 +1,12 @@
+
+public class SalesData {
+
+	
+	int data = 5;
+	
+	
+	public void display(){
+		
+		System.out.println("Esto es todo por hoy!" );
+	}
+}
